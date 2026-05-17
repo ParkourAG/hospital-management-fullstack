8291c384d6a4885e48c794ff86b9b5ac4d56bc81
ParkourAG <guchhaitanubrata@gmail.com>
2026-05-17 13:25:49 +0530

    .

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { clerkMiddleware } from '@clerk/express';
+
+const app= express();
+const port= 4000; 
+
+// MiddleWares
+app.use(cors());
+app.use(clerkMiddleware());
+app.use(express.json({limit: "20mb"}));
+app.use(express.urlencoded({limit: "20mb", extended: true}));
+
+// DB
+
+// Routes
+app.get('/', (req,res)=>{
+    res.send("API Working...");
+});
+
+app.listen(port, ()=>{
+    console.log(`server started on http://localhost:${port}`);
+});
